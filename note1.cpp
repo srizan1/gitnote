961de9aab15c8365c581eb736fabbba8e5bd67d6
Srizan Pallapothu <gntosnb09pallapothusrizan@gmail.com>
2025-05-29 23:51:45 +0530

    v5 clone from another pc

--- note1.cpp
+++ note1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"location gitnote1"<<"version 5.1"<<endl;
+  cout<<"location gitnote1"<<"version 5.1"<<"clone"<<endl;
     return 0;
 }
--- note2.cpp
+++ note2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-  cout<<"location gitnote2"<<"version 4.1"<<endl;
+  cout<<"location gitnote2"<<"version 4.1"<<"gitnoteclone"<<endl;
     return 0;
 }
