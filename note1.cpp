118d146021f9e7a7f7bceeca788fe6ee64c213ff
Srizan Pallapothu <gntosnb09pallapothusrizan@gmail.com>
2025-05-29 10:30:39 +0530

    v1

---
+++ note1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"location gitnote1"<<"version 1"<<endl;
+    return 0;
+}
---
+++ note2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"location gitnote2"<<"version 1"<<endl;
+    return 0;
+}
